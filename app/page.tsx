3aef1679a24e5fe1a08ac1ed115685515a2df57e
hanidev08 <hanidev08@gmail.com>
2025-04-28 11:35:45 +0100

    Add acuityscheduling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className=" flex h-screen  justify-center items-center">
+      <iframe
+        src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
+        width={750}
+        height={500}
+      />
+    </div>
+  );
 }
+{/* <iframe
+  src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
+  width={750}
+  height={500}
+/>; */}
