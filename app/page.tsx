7b45c6299bb96412ce4e12b3e9d2edd64d0ccc68
hanidev08 <hanidev08@gmail.com>
2025-05-14 20:25:33 +0100

    Delete new

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <>
-      <h1 className=" text-center">Hello World new</h1>
+      <h1 className=" text-center">Hello World</h1>
       <div className=" flex h-screen  justify-center items-center">
         <iframe
-          src="https://app.acuityscheduling.com/schedule.php?owner=35575719"
+          src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
           width={750}
           height={500}
         />
@@ -21,5 +21,3 @@ export default function Home() {
 
 */
 }
-
-//           // src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
