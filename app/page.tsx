968841a00598ed1bfade0286ef6344428fbd883f
hanidev08 <hanidev08@gmail.com>
2025-05-14 20:15:25 +0100

    Add font  Perfectly 

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 export default function Home() {
   return (
-    <div className=" flex h-screen  justify-center items-center">
-      <iframe
-        src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
-        width={750}
-        height={500}
-      />
-    </div>
+    <>
+      <div className=" flex h-screen  justify-center items-center">
+        <iframe
+          src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
+          width={750}
+          height={500}
+        />
+      </div>
+      <h1 className=" text-center">Hello World</h1>
+    </>
   );
 }
-{/* <iframe
+{
+  /* <iframe
   src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
   width={750}
   height={500}
-/>; */}
+/>; */
+}
