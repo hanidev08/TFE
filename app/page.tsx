99e7448450f92b2512d023ef635501a66f2f87a8
hanidev08 <hanidev08@gmail.com>
2025-05-14 20:16:48 +0100

    Add new Font

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <>
+      <h1 className=" text-center">Hello World</h1>
       <div className=" flex h-screen  justify-center items-center">
         <iframe
           src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
@@ -8,7 +9,6 @@ export default function Home() {
           height={500}
         />
       </div>
-      <h1 className=" text-center">Hello World</h1>
     </>
   );
 }
