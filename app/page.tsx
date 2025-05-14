2c53246b57d3b761383afb8aad4fe614cf1b980f
hanidev08 <hanidev08@gmail.com>
2025-05-14 20:22:04 +0100

    Add new words

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ export default function Home() {
       <h1 className=" text-center">Hello World new</h1>
       <div className=" flex h-screen  justify-center items-center">
         <iframe
-          src="https://app.acuityscheduling.com/schedule.php?owner=35575719"
-          // src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
-
+          src="https://app.acuityscheduling.com/schedule/e9fbf3f6/appointment/77471924/calendar/11984339"
           width={750}
           height={500}
         />
